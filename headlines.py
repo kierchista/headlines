70e54dcb6bea368dbfb05cd742422e09ad0b4add
pabloli150 <pablo.perez1981@gmail.com>
2017-04-11 13:53:15 +0000

    v4

--- headlines.py
+++ headlines.py
@@ -10,7 +10,7 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 @app.route("/")
 @app.route("/<publication>")
-def get_news(publication):
+def get_news(publication="bbc"):
   feed = feedparser.parse(RSS_FEEDS[publication])
   first_article = feed['entries'][0]
   return """<html>
